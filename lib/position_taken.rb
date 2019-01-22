e465ff69731f6cce9784451847144c0bae94803e
Lisa Webb <lwebbnyc@outlook.com>
2019-01-22 20:18:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
 def position_taken?(board,index)
   if board[index] == " " || == ""
     false
-  elsif board[index] == ""
-    false
-  elsif board[index] == nil
+   elsif board[index] == nil
     nil
   else
     true
